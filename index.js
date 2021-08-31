6cc3456d8476efc6d7946a981172fd8b21cb3615
Warus15 <63123542+Warus15@users.noreply.github.com>
2021-08-31 22:58:38 +0200

    express

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const http = require("http");
-const colors = require("colors");
-
-const hostname = "127.0.0.1";
+const express = require("express");
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+//Additional consts
+const colors = require("colors");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`.green);
+app.listen(port, () => {
+  console.log(`Server running at http://127.0.0.1:${port}/`.green);
 });
