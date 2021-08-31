500506f5f3700a306d54da2f7b7254bddbdebbfe
Warus15 <63123542+Warus15@users.noreply.github.com>
2021-08-31 23:33:19 +0200

    Migration to express

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# jupikpoly
-Monopoly-type game using Node.js, Socket.io and Three.js.
+# jupikpoly-server
+
+Server for a monopoly-type game using Node.js, Socket.io and Three.js.
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const socket = require("socket.io");
+const path = require("path");
+
 const app = express();
 const port = 3000;
 
 //Additional consts
 const colors = require("colors");
 
+//Requests
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+//
+const server = app.listen(port, () => {
   console.log(`Server running at http://127.0.0.1:${port}/`.green);
 });
+
+const io = socket(server);
+
+io.on("connection", socket => {});
